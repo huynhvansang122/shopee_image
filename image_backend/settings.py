2e6dc598882fc30cb183e0bd26efadfcebbb2afc
ngockhue1620 <ngockhuentca2k@gmail.com>
2021-09-26 19:29:03 +0700

    add

--- image_backend/settings.py
+++ image_backend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ed-l=nu079n1_k=tvqqctv_a8!qqrj#$i0s=g*n@l=uxk(miiw'
+SECRET_KEY = os.environ['SECRET_KEY'] if 'SECRET_KEY' in os.environ else env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
